f5b37556aff86f3cfc26515932ad965ef5da2929
Nayil111 <94451293+Nayil111@users.noreply.github.com>
2022-12-01 16:45:26 +0300

    Add files via upload

---
+++ Task manager/OS lab 1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Windows.Forms;
+
+namespace OS_lab_1
+{
+    public partial class Form1 : Form
+    {
+        private object lblCount;
+        private object Text;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowProcesses();
+        }
+        private void ShowProcesses()
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                AddProcess(process);
+            }
+            lblCount.Text = "Количество процессов: " + dgvProcess.Rows.Count.ToString();
+        }
+        private void AddProcess(Process process)
+        {
+            String[] array = new String[6];
+            array[0] = process.Id.ToString();
+            array[1] = process.ProcessName;
+            array[2] = process.VirtualMemorySize64.ToString();
+            array[3] = GetProcessUser(process);
+            array[4] = process.BasePriority.ToString();
+            array[5] = process.Threads.Count.ToString();
+            dgvProcess.Rows.Add(array);
+        }
+        private void dgvProcess_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int index = e.RowIndex;
+            DataGridViewRow row = dgvProcess.Rows[index];
+            Process p = Process.GetProcessById(Convert.ToInt32(row.Cells[0].Value));
+            if (p.Threads.Count <= 0)
+            {
+                MessageBox.Show("У данного процесса нет потоков");
+                return;
+            }
+            lblCount.Text = p.ToString();
+            Threadscs form = new Threadscs(p);
+            form.Show();
+        }
+        private static string GetProcessUser(Process process)
+        {
+            IntPtr processHandle = IntPtr.Zero;
+            try
+            {
+                OpenProcessToken(process.Handle, 8, out processHandle);
+                WindowsIdentity wi = new WindowsIdentity(processHandle);
+                string user = wi.Name;
+                return user.Contains(@"\") ? user.Substring(user.IndexOf(@"\", StringComparison.Ordinal) + 1) : user;
+            }
+            catch
+            {
+                return null;
+            }
+            finally
+            {
+                if (processHandle != IntPtr.Zero)
+                {
+                    CloseHandle(processHandle);
+                }
+            }
+        }
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool OpenProcessToken(IntPtr processHandle, uint desiredAccess, out IntPtr tokenHandle);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            dgvProcess.Rows.Clear();
+            ShowProcesses();
+        }
+
+        private void dgvProcess_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Task manager/OS lab 1/Threadscs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace OS_lab_1
+{
+    public partial class Threadscs : Form
+    {
+        public Threadscs()
+        {
+            InitializeComponent();
+        }
+        public Threadscs(Process proc)
+        {
+            InitializeComponent();
+            ShowThreads(proc);
+        }
+        private void ShowThreads (Process proc)
+        {
+            lblID.Text = "ID процесса: " + proc.Id.ToString();
+            lblName.Text = "Имя процесса: " + proc.ProcessName;
+            foreach (ProcessThread thread in proc.Threads)
+            {
+                dgvThreads.Rows.Add(thread.Id, thread.CurrentPriority);
+            } 
+        }
+    }
+}
